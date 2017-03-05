a1d8078bad429742a540da65705cb89e9005ac8e
David Thomas <david.thomas214@yahoo.com>
2017-03-05 10:36:59 -0800

    Bug Fix
    - Swapping BaseApp from interface to abstract

--- framework/app/BaseApp.php
+++ framework/app/BaseApp.php
@@ -1,14 +1,14 @@
 <?php
 namespace Framework\App;
-interface BaseApp {
-	private function __construct();
-	public function setResponse(\Framework\Model\Inet\Response\Response $Response);
-	public function setResponseCode(int $code);
-	public function setHeader(string $property, string $value);
-	public function setData(string $data);
-	public function getAppName(): string;
-	protected function config(\stdClass $config);
-	public function processRequest(\Framework\Model\Inet\Request\Request $Request);
-	public function getAppUrl(string $path = ""): string;
-	public function getPath(string $path = ""):string;
+abstract class BaseApp {
+	private final function __construct();
+	abstract public function setResponse(\Framework\Model\Inet\Response\Response $Response);
+	abstract public function setResponseCode(int $code);
+	abstract public function setHeader(string $property, string $value);
+	abstract public function setData(string $data);
+	abstract public function getAppName(): string;
+	abstract protected function config(\stdClass $config);
+	abstract public function processRequest(\Framework\Model\Inet\Request\Request $Request);
+	abstract public function getAppUrl(string $path = ""): string;
+	abstract public function getPath(string $path = ""):string;
 }
