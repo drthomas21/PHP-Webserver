e2e0e876cefd371df7ec7383b5ef98f8a1f77c3a
David Thomas <david.thomas214@yahoo.com>
2017-04-15 15:20:57 -0700

    code sync

--- framework/model/inet/response/Response.php
+++ framework/model/inet/response/Response.php
@@ -5,7 +5,7 @@ abstract class Response {
 	abstract function sendResponse();
 	abstract function sendContent(string $content);
 	abstract function sendHeaders();
-	public function setThread(\Framework\Thread\ChildThread $thread) {
+	public function setThread(\App\Webserver\ChildThread $thread) {
 		$this->thread = $thread;
 	}
 	public function isClientConnected(): bool {
--- framework/provider/routing/RoutingProvider.php
+++ framework/provider/routing/RoutingProvider.php
@@ -6,7 +6,7 @@ class RoutingProvider {
 		self::$apps[$App->getHostname()] = $App;
 	}
 
-	public static function processRequest(\Framework\Model\Inet\Request\Request $Request, \Framework\Thread\ChildThread $Thread):\Framework\Model\Inet\Response\Response {
+	public static function processRequest(\Framework\Model\Inet\Request\Request $Request, \App\Webserver\ChildThread $Thread):\Framework\Model\Inet\Response\Response {
 		$type = strtoupper(str_replace("Request","",array_slice(explode('\\',get_class($Request)),-1)[0]));
 		$Response = \Framework\Factory\Inet\ResponseBuilder::buildResponse("{$type}/{$Request->version}");
 		$Response->setThread($Thread);
